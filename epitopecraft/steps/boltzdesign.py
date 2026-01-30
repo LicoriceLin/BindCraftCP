488032b8d7c37aa3c0058f6b7daabd66ec0aa2d4
Zhenfeng Evan Deng <zfevan.deng@mail.utoronto.ca>
2026-01-30 17:36:09 -0500

    ckpt0130

--- epitopecraft/steps/boltzdesign.py
+++ epitopecraft/steps/boltzdesign.py
@@ -3,7 +3,7 @@ from epitopecraft.steps.scorer.pymol_utils import *
 from pymol import cmd
 import yaml
 from subprocess import run
-
+from warnings import warn
 class BolzGenSampler(BaseStep):
     def __init__(self,
         settings:GlobalSettings,
@@ -15,10 +15,6 @@ class BolzGenSampler(BaseStep):
         3. num_designs=len(bs.binder_lengths) * len(bs.helix_values) * len(bs.global_seed)
         '''
         super().__init__(settings)
-        design_path=self.settings.binder_settings.design_path
-        design_name=self.settings.binder_settings.binder_name
-        Path(design_path).mkdir(exist_ok=True,parents=True)
-        self.yaml_recipe=f'{design_path}/{design_name}-boltz.yaml'
 
     def _gen_sub(self):
         adv=self.settings.adv
@@ -38,12 +34,18 @@ class BolzGenSampler(BaseStep):
 
         # hotspot params
         hotspot_str=self.settings.target_settings.target_hotspot_residues
-        hotspots:List[Tuple[str,str]]=[(i[0],i[1:]) for i in hotspot_str.split(',')]
-        hsp_dict={}
-        for i in hotspots:
-            _=hsp_dict.setdefault(i[0],[])
-            _.append(id_map[i[0]][i[1]])
-        binding_types=[{'chain':{'id':k,'binding':','.join(v)}} for k,v in hsp_dict.items()]
+        epitope_range:str=adv.setdefault('epitope_range','full')
+        epitope_strategy:str=adv.setdefault('epitope_strategy','none') 
+        if hotspot_str:
+            hotspots:List[Tuple[str,str]]=[(i[0],i[1:]) for i in hotspot_str.split(',')]
+            hsp_dict={}
+            for i in hotspots:
+                _=hsp_dict.setdefault(i[0],[])
+                _.append(id_map[i[0]][i[1]])
+            binding_types=[{'chain':{'id':k,'binding':','.join(v)}} for k,v in hsp_dict.items()]
+        else:
+            binding_types=[]
+            assert epitope_range=='full', 'epitope-only strategy attempted without hotspots specified.'
 
         # binder params
         cyclic=adv.setdefault('cyclize_peptide',False)
@@ -53,7 +55,6 @@ class BolzGenSampler(BaseStep):
          'sequence': binder_length_range, 'cyclic': cyclic}
         
         # epitope params
-        epitope_range:str=adv.setdefault('epitope_range','full')
         if epitope_range =='full':
             chain=[{'chain':{'id':c}} for c in target_chain.split(',')]
         else:
@@ -61,7 +62,6 @@ class BolzGenSampler(BaseStep):
                 epitope_range=int(epitope_range)
             except:
                 raise ValueError(f'invalid epitope range: {epitope_range}')
-            epitope_strategy:str=adv.setdefault('epitope_strategy','top-k') 
             if epitope_strategy == 'top-k':
                 cmd.create('target',f'ori and (chain {target_chain})')
                 other_res_sorted_list=sort_distance_to_hotspots('target',hotspots)
@@ -85,23 +85,21 @@ class BolzGenSampler(BaseStep):
             else:
                 raise ValueError(f'invalid `epitope_strategy`:{epitope_strategy}')
 
-        yaml_opt={'entities': 
-            [{'file': 
-                {'path': str(Path(full_pdb).absolute()),
+        file_d={'path': str(Path(full_pdb).absolute()),
                 'include': chain,
                 'structure_groups': 'all',
-                'binding_types':binding_types
                 }
-                },
+        if binding_types:
+            file_d['binding_types']=binding_types
+        yaml_opt={'entities': 
+            [{'file': file_d},
             {'protein': binder_d
-            }]
-                }
-        design_path=self.settings.binder_settings.design_path
-        design_name=self.settings.binder_settings.binder_name
-        Path(design_path).mkdir(exist_ok=True,parents=True)
-        with open(self.yaml_recipe,'w') as f:
+            }]}
+        Path(self.design_path).mkdir(exist_ok=True,parents=True)
+        yaml_recipe=f'{self.design_path}/{self.design_name}-boltz.yaml'
+        with open(yaml_recipe,'w') as f:
             f.write(yaml.safe_dump(yaml_opt,indent=2,sort_keys=False))
-        cmd.save(self.yaml_recipe.replace('.yaml','.pse'))
+        cmd.save(yaml_recipe.replace('.yaml','.pse'))
         for i in objects:
             cmd.delete(i)
 
@@ -118,22 +116,23 @@ class BolzGenSampler(BaseStep):
         else:
             protocol_='peptide-anything'
         num_designs=len(bs.binder_lengths) * len(bs.helix_values) * len(bs.random_seeds)
-
+        yaml_recipe=f'{self.design_path}/{self.design_name}-boltz.yaml'
         
         run(['conda','run',flag,env, 
-            'boltzgen','run',self.yaml_recipe, 
+            'boltzgen','run',yaml_recipe, 
             '--output',bs.design_path+'/boltzdesign',
             '--protocol',protocol_, '--num_designs', str(num_designs), '--budget', str(num_designs), '--reuse'])
-
+        self.settings.target_settings.starting_pdb=f'{bs.design_path}/boltzdesign/{bs.binder_name}-boltz.cif'
 
     def collect_results(self):
-        pass
+        assert self.metrics is not None
+        
 
     @property
     def params_to_take(self)->Tuple[str,...]:
         '''
         epitope_range: "full" or a int number
-        epitope_strategy: top-k or dist-range
+        epitope_strategy: "none" or "top-k" or "dist-range" 
         '''
         ret=['epitope_range','epitope_strategy']
         return tuple(ret)
@@ -143,8 +142,31 @@ class BolzGenSampler(BaseStep):
         return 'boltzgen'
     
     def process_record(self):
-        pass
+        raise NotImplementedError(f'Run {self.name} in Batches!')
 
+    @property
+    def design_path(self):
+        return self.settings.binder_settings.design_path
+
+    @property
+    def design_name(self):
+        return self.settings.binder_settings.binder_name
+
+    @property
+    def metrics(self):
+        '''
+        trick: del `self._metrics` to reload.
+        '''
+        if getattr(self,'_metrics', None) is None:
+            metrics_path=f'{self.design_path}/boltzdesign/final_ranked_designs/all_designs_metrics.csv'
+            if Path(metrics_path).exists():
+                self._metrics=pd.read_csv(metrics_path)
+            else:
+                warn(f'metrics not found at {metrics_path}')
+                self._metrics = None
+        return self._metrics
+
+            
         
 
 
